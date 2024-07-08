b4f019a189e5bc45c5a817db35693d10ad2c17cb
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2024-07-09 04:43:11 +0700

    Create Main.php

---
+++ src/NurAzliYT/QuickCash/Main.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace NurAzliYT\QuickCash;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\PluginCommand;
+use NurAzliYT\QuickCash\command\{
+    MyMoneyCommand, MyDebtCommand, TakeDebtCommand, ReturnDebtCommand, 
+    TopMoneyCommand, MoneySaveCommand, MoneyLoadCommand, SetMoneyCommand, 
+    GiveMoneyCommand, TakeMoneyCommand, SeeMoneyCommand
+};
+
+class Main extends PluginBase {
+
+    /** @var PlayerData */
+    private $playerData;
+
+    public function onEnable() {
+        $this->getLogger()->info("QuickCash has been enabled!");
+        @mkdir($this->getDataFolder());
+        $this->playerData = new PlayerData($this);
+
+        $this->registerCommands();
+    }
+
+    public function onDisable() {
+        $this->getLogger()->info("QuickCash has been disabled.");
+    }
+
+    private function registerCommands() {
+        $this->getServer()->getCommandMap()->registerAll("quickcash", [
+            new PluginCommand("mymoney", $this, new MyMoneyCommand($this)),
+            new PluginCommand("mydebt", $this, new MyDebtCommand($this)),
+            new PluginCommand("takedebt", $this, new TakeDebtCommand($this)),
+            new PluginCommand("returndebt", $this, new ReturnDebtCommand($this)),
+            new PluginCommand("topmoney", $this, new TopMoneyCommand($this)),
+            new PluginCommand("moneysave", $this, new MoneySaveCommand($this)),
+            new PluginCommand("moneyload", $this, new MoneyLoadCommand($this)),
+            new PluginCommand("setmoney", $this, new SetMoneyCommand($this)),
+            new PluginCommand("givemoney", $this, new GiveMoneyCommand($this)),
+            new PluginCommand("takemoney", $this, new TakeMoneyCommand($this)),
+            new PluginCommand("seemoney", $this, new SeeMoneyCommand($this)),
+        ]);
+    }
+
+    public function getPlayerData(): PlayerData {
+        return $this->playerData;
+    }
+}
