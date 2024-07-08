890a59ca3adbe5ccaea9e128afbb716c3698272b
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2024-07-09 04:44:45 +0700

    Create MyMoneyCommand.php

---
+++ src/NurAzliYT/QuickCash/command/MyMoneyCommand.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace NurAzliYT\QuickCash\command;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginOwned;
+use pocketmine\plugin\PluginOwnedTrait;
+use pocketmine\player\Player;
+use NurAzliYT\QuickCash\Main;
+
+class MyMoneyCommand extends Command implements PluginOwned {
+    use PluginOwnedTrait;
+
+    public function __construct(Main $plugin) {
+        $this->owningPlugin = $plugin;
+        parent::__construct("mymoney", "Shows your money", "/mymoney");
+    }
+
+    public function execute(CommandSender $sender, string $label, array $args): bool {
+        if($sender instanceof Player) {
+            $money = $this->owningPlugin->getPlayerData()->getMoney($sender->getName());
+            $sender->sendMessage("Your money: $" . $money);
+        } else {
+            $sender->sendMessage("This command can only be used by players.");
+        }
+        return true;
+    }
+}
