543fd87b5f5ebe064a0a86b5d8c4c14818bf74cf
XPocketMC <tvfinitoflamboyan@gmail.com>
2024-07-10 04:50:39 +0700

    Update SetMoneyCommand.php

--- src/NurAzliYT/QuickCash/command/SetMoneyCommand.php
+++ src/NurAzliYT/QuickCash/command/SetMoneyCommand.php
@@ -14,7 +14,7 @@ class SetMoneyCommand extends Command implements PluginOwned {
     private Main $plugin;
 
     public function __construct(Main $plugin) {
-        parent::__construct("setmoney", "Sets a player's money", "/setmoney <player> <money>");
+        parent::__construct("setmoney", "Sets a player's money", "/setmoney <player> <money>", ["setmoney"]);
         $this->setPermission("quickcash.command.setmoney");
         $this->plugin = $plugin;
     }
