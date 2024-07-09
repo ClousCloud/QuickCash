90a2c1e4f018268b972087f37941c9393e96e3b2
XPocketMC <tvfinitoflamboyan@gmail.com>
2024-07-09 22:00:26 +0700

    Update Main.php

--- src/NurAzliYT/QuickCash/Main.php
+++ src/NurAzliYT/QuickCash/Main.php
@@ -4,52 +4,43 @@ namespace NurAzliYT\QuickCash;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
-use pocketmine\command\PluginCommand;
-use pocketmine\utils\Config;
-use NurAzliYT\QuickCash\data\PlayerData;
-use NurAzliYT\QuickCash\command\MyMoneyCommand;
-use NurAzliYT\QuickCash\command\MyDebtCommand;
-use NurAzliYT\QuickCash\command\TakeDebtCommand;
-use NurAzliYT\QuickCash\command\ReturnDebtCommand;
-use NurAzliYT\QuickCash\command\TopMoneyCommand;
-use NurAzliYT\QuickCash\command\MoneySaveCommand;
-use NurAzliYT\QuickCash\command\MoneyLoadCommand;
-use NurAzliYT\QuickCash\command\SetMoneyCommand;
-use NurAzliYT\QuickCash\command\GiveMoneyCommand;
-use NurAzliYT\QuickCash\command\TakeMoneyCommand;
-use NurAzliYT\QuickCash\command\SeeMoneyCommand;
+use NurAzliYT\QuickCash\command\{
+    MyMoneyCommand,
+    MyDebtCommand,
+    TakeDebtCommand,
+    ReturnDebtCommand,
+    TopMoneyCommand,
+    MoneySaveCommand,
+    MoneyLoadCommand,
+    SetMoneyCommand,
+    GiveMoneyCommand,
+    TakeMoneyCommand,
+    SeeMoneyCommand
+};
 
 class Main extends PluginBase {
-
     private PlayerData $playerData;
 
-    public function onEnable(): void {
+    protected function onEnable(): void {
         $this->saveDefaultConfig();
-        $this->playerData = new PlayerData($this->getDataFolder());
+        $this->playerData = new PlayerData($this);
 
-        $this->registerCommands();
+        $this->registerCommand(new MyMoneyCommand($this));
+        $this->registerCommand(new MyMoneyCommand($this));
+        $this->registerCommand(new MyDebtCommand($this));
+        $this->registerCommand(new TakeDebtCommand($this));
+        $this->registerCommand(new ReturnDebtCommand($this));
+        $this->registerCommand(new TopMoneyCommand($this));
+        $this->registerCommand(new MoneySaveCommand($this));
+        $this->registerCommand(new MoneyLoadCommand($this));
+        $this->registerCommand(new SetMoneyCommand($this));
+        $this->registerCommand(new GiveMoneyCommand($this));
+        $this->registerCommand(new TakeMoneyCommand($this));
+        $this->registerCommand(new SeeMoneyCommand($this));
     }
 
-    private function registerCommands(): void {
-        $commandMap = $this->getServer()->getCommandMap();
-        
-        $commands = [
-            new MyMoneyCommand($this),
-            new MyDebtCommand($this),
-            new TakeDebtCommand($this),
-            new ReturnDebtCommand($this),
-            new TopMoneyCommand($this),
-            new MoneySaveCommand($this),
-            new MoneyLoadCommand($this),
-            new SetMoneyCommand($this),
-            new GiveMoneyCommand($this),
-            new TakeMoneyCommand($this),
-            new SeeMoneyCommand($this)
-        ];
-
-        foreach ($commands as $command) {
-            $commandMap->register($command->getName(), $command);
-        }
+    private function registerCommand(Command $command): void {
+        $this->getServer()->getCommandMap()->register($command->getName(), $command);
     }
 
     public function getPlayerData(): PlayerData {
