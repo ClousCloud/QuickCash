8a16d082387e1bee14a8b4af6f511b8daf841e0c
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2024-07-09 21:20:59 +0700

    Update TakeMoneyCommand.php

--- src/NurAzliYT/QuickCash/command/TakeMoneyCommand.php
+++ src/NurAzliYT/QuickCash/command/TakeMoneyCommand.php
@@ -14,26 +14,29 @@ class TakeMoneyCommand extends Command implements PluginOwned {
     private Main $plugin;
 
     public function __construct(Main $plugin) {
-        $this->plugin = $plugin;
-        $this->owningPlugin = $plugin;
         parent::__construct("takemoney", "Takes money from a player", "/takemoney <player> <money>");
-        $this->setPermission("quickcash.admin");
+        $this->setPermission("quickcash.command.takemoney");
+        $this->plugin = $plugin;
     }
 
-    public function execute(CommandSender $sender, string $label, array $args): bool {
-        if($sender->hasPermission("quickcash.admin")) {
-            if(count($args) !== 2 || !is_numeric($args[1])) {
-                $sender->sendMessage("Usage: /takemoney <player> <money>");
-                return false;
-            }
-
-            $player = $args[0];
-            $amount = floatval($args[1]);
-            $this->plugin->reduceMoney($player, $amount);
-            $sender->sendMessage("Took $" . $amount . " from " . $player . ".");
-        } else {
-            $sender->sendMessage("You don't have permission to use this command.");
+    public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
+        if (!$this->testPermission($sender)) {
+            return false;
         }
+
+        if (count($args) !== 2 || !is_numeric($args[1])) {
+            $sender->sendMessage("Usage: /takemoney <player> <money>");
+            return false;
+        }
+
+        $player = $args[0];
+        $amount = (int)$args[1];
+        $this->plugin->getPlayerData()->takeMoney($player, $amount);
+        $sender->sendMessage("Took $$amount from $player");
         return true;
+    }
+
+    public function getOwningPlugin(): Main {
+        return $this->plugin;
     }
 }
