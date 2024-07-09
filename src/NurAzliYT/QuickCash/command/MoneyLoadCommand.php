609b122f1cc9f9967c6a3541aaea604ffd089706
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2024-07-09 15:16:13 +0700

    Update MoneyLoadCommand.php

--- src/NurAzliYT/QuickCash/command/MoneyLoadCommand.php
+++ src/NurAzliYT/QuickCash/command/MoneyLoadCommand.php
@@ -11,16 +11,19 @@ use NurAzliYT\QuickCash\Main;
 class MoneyLoadCommand extends Command implements PluginOwned {
     use PluginOwnedTrait;
 
+    private Main $plugin;
+
     public function __construct(Main $plugin) {
+        $this->plugin = $plugin;
         $this->owningPlugin = $plugin;
         parent::__construct("moneyload", "Loads data from your hardware", "/moneyload");
-        $this->setPermission("quickcash.admin");
+        $this->setPermission("quickcash.console");
     }
 
     public function execute(CommandSender $sender, string $label, array $args): bool {
-        if($sender->hasPermission("quickcash.admin")) {
-            $this->owningPlugin->getPlayerData()->load();
-            $sender->sendMessage("Player data has been loaded.");
+        if($sender->hasPermission("quickcash.console")) {
+            $this->plugin->getPlayerData()->reload();
+            $sender->sendMessage("Player data loaded from hardware.");
         } else {
             $sender->sendMessage("You don't have permission to use this command.");
         }
