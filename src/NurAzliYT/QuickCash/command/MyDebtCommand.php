4022edd151b32151af0b4249cb9d6516cdc55ac4
NurAzliYT <135945845+NurAzliYT@users.noreply.github.com>
2024-07-09 21:12:39 +0700

    Update MyDebtCommand.php

--- src/NurAzliYT/QuickCash/command/MyDebtCommand.php
+++ src/NurAzliYT/QuickCash/command/MyDebtCommand.php
@@ -4,9 +4,9 @@ namespace NurAzliYT\QuickCash\command;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
+use pocketmine\player\Player;
 use pocketmine\plugin\PluginOwned;
 use pocketmine\plugin\PluginOwnedTrait;
-use pocketmine\player\Player;
 use NurAzliYT\QuickCash\Main;
 
 class MyDebtCommand extends Command implements PluginOwned {
@@ -15,19 +15,27 @@ class MyDebtCommand extends Command implements PluginOwned {
     private Main $plugin;
 
     public function __construct(Main $plugin) {
-        $this->plugin = $plugin;
-        $this->owningPlugin = $plugin;
         parent::__construct("mydebt", "Shows your debt", "/mydebt");
         $this->setPermission("quickcash.command.mydebt");
+        $this->plugin = $plugin;
     }
 
-    public function execute(CommandSender $sender, string $label, array $args): bool {
-        if($sender instanceof Player) {
-            $debt = $this->plugin->getDebt($sender->getName());
-            $sender->sendMessage("Your current debt: $" . $debt);
-        } else {
-            $sender->sendMessage("This command can only be used in-game.");
+    public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
+        if (!$this->testPermission($sender)) {
+            return false;
         }
+
+        if (!$sender instanceof Player) {
+            $sender->sendMessage("This command can only be used in-game.");
+            return false;
+        }
+
+        $debt = $this->plugin->getPlayerData()->getDebt($sender->getName());
+        $sender->sendMessage("You have a debt of $" . $debt);
         return true;
+    }
+
+    public function getOwningPlugin(): Main {
+        return $this->plugin;
     }
 }
