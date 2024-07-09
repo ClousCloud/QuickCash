c6442f094ee0d1b4370983d8b0d7e6e98dac12ee
XPocketMC <tvfinitoflamboyan@gmail.com>
2024-07-10 04:37:32 +0700

    Update Main.php

--- src/NurAzliYT/QuickCash/Main.php
+++ src/NurAzliYT/QuickCash/Main.php
@@ -1,32 +1,31 @@
 <?php
 
+declare(strict_types=1);
+
 namespace NurAzliYT\QuickCash;
 
 use pocketmine\plugin\PluginBase;
 use pocketmine\command\Command;
-use NurAzliYT\QuickCash\command\{
-    MyMoneyCommand,
-    MyDebtCommand,
-    TakeDebtCommand,
-    ReturnDebtCommand,
-    TopMoneyCommand,
-    MoneySaveCommand,
-    MoneyLoadCommand,
-    SetMoneyCommand,
-    GiveMoneyCommand,
-    TakeMoneyCommand,
-    SeeMoneyCommand
-};
-use NurAzliYT\QuickCash\data\PlayerData;
+use pocketmine\command\PluginCommand;
+use NurAzliYT\QuickCash\command\MyMoneyCommand;
+use NurAzliYT\QuickCash\command\MyDebtCommand;
+use NurAzliYT\QuickCash\command\TakeDebtCommand;
+use NurAzliYT\QuickCash\command\ReturnDebtCommand;
+use NurAzliYT\QuickCash\command\TopMoneyCommand;
+use NurAzliYT\QuickCash\command\MoneySaveCommand;
+use NurAzliYT\QuickCash\command\MoneyLoadCommand;
+use NurAzliYT\QuickCash\command\SetMoneyCommand;
+use NurAzliYT\QuickCash\command\GiveMoneyCommand;
+use NurAzliYT\QuickCash\command\TakeMoneyCommand;
+use NurAzliYT\QuickCash\command\SeeMoneyCommand;
 
 class Main extends PluginBase {
+
     private PlayerData $playerData;
 
-    protected function onEnable(): void {
-        $this->saveDefaultConfig();
+    public function onEnable(): void {
         $this->playerData = new PlayerData($this);
 
-        $this->registerCommand(new MyMoneyCommand($this));
         $this->registerCommand(new MyMoneyCommand($this));
         $this->registerCommand(new MyDebtCommand($this));
         $this->registerCommand(new TakeDebtCommand($this));
@@ -41,7 +40,10 @@ class Main extends PluginBase {
     }
 
     private function registerCommand(Command $command): void {
-        $this->getServer()->getCommandMap()->register($command->getName(), $command);
+        if ($command instanceof PluginCommand) {
+            $command->setPermissionMessage("You do not have permission to use this command");
+        }
+        $this->getServer()->getCommandMap()->register($this->getName(), $command);
     }
 
     public function getPlayerData(): PlayerData {
