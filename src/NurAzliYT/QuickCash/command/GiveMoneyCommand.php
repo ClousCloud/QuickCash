d77b882aa130b0001bb849d7071bad8fb2f28c91
XPocketMC <tvfinitoflamboyan@gmail.com>
2024-07-10 14:29:00 +0700

    Update GiveMoneyCommand.php

--- src/NurAzliYT/QuickCash/command/GiveMoneyCommand.php
+++ src/NurAzliYT/QuickCash/command/GiveMoneyCommand.php
@@ -1,23 +1,24 @@
 <?php
 
+declare(strict_types=1);
+
 namespace NurAzliYT\QuickCash\command;
 
-use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 use pocketmine\command\PluginCommand;
 use pocketmine\plugin\PluginOwned;
 use pocketmine\plugin\PluginOwnedTrait;
+use pocketmine\player\Player;
 use NurAzliYT\QuickCash\Main;
 
 class GiveMoneyCommand extends PluginCommand implements PluginOwned {
     use PluginOwnedTrait;
 
-    private Main $plugin;
-
     public function __construct(Main $plugin) {
-        parent::__construct("givemoney", "Gives money to a player", "/givemoney <player> <money>", ["givemoney"]);
+        parent::__construct("givemoney", $plugin);
+        $this->setDescription("Gives money to a player");
         $this->setPermission("quickcash.command.givemoney");
-        $this->plugin = $plugin;
+        $this->owningPlugin = $plugin;
     }
 
     public function execute(CommandSender $sender, string $commandLabel, array $args): bool {
@@ -25,19 +26,23 @@ class GiveMoneyCommand extends PluginCommand implements PluginOwned {
             return false;
         }
 
-        if (count($args) !== 2 || !is_numeric($args[1])) {
+        if (count($args) < 2 || !is_numeric($args[1])) {
             $sender->sendMessage("Usage: /givemoney <player> <money>");
             return false;
         }
 
-        $player = $args[0];
-        $amount = (int)$args[1];
-        $this->plugin->getPlayerData()->addMoney($player, $amount);
-        $sender->sendMessage("Gave $$amount to $player");
-        return true;
-    }
+        $playerName = $args[0];
+        $amount = (int) $args[1];
+        $player = $this->owningPlugin->getServer()->getPlayerByPrefix($playerName);
 
-    public function getOwningPlugin(): Main {
-        return $this->plugin;
+        if ($player instanceof Player) {
+            $this->owningPlugin->getPlayerData()->addMoney($player, $amount);
+            $sender->sendMessage("Gave $" . $amount . " to " . $player->getName());
+            $player->sendMessage("You have received $" . $amount);
+            return true;
+        } else {
+            $sender->sendMessage("Player not found.");
+            return false;
+        }
     }
 }
